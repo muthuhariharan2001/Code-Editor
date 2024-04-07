0609844cc5a0d37440e0403bb624a9b0fdeb2ee5
muthuhariharan2001 <164756456+muthuhariharan2001@users.noreply.github.com>
2024-04-07 14:13:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,6 @@ function run() {
     let jsCode = document.getElementById("js-code");
     let output = document.getElementById("output");
 
-    // Save code to local storage
-    localStorage.setItem('htmlCode', htmlCode.value);
-    localStorage.setItem('cssCode', cssCode.value);
-    localStorage.setItem('jsCode', jsCode.value);
-
-    // Retrieve code from local storage
-    let savedHtml = localStorage.getItem('htmlCode') || '';
-    let savedCss = localStorage.getItem('cssCode') || '';
-    let savedJs = localStorage.getItem('jsCode') || '';
-
-    // Display output
-    output.contentDocument.body.innerHTML = savedHtml + "<style>" + savedCss + "</style>";
-    output.contentWindow.eval(savedJs);
+    output.contentDocument.body.innerHTML = htmlCode.value + "<style>" + cssCode.value + "</style>";
+    output.contentWindow.eval(jsCode.value);
 }
